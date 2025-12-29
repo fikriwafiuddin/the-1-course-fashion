787ba8f74e097ee64c28757a6897a245a063be62
fikriwafiuddin <fikriwafiuddin913@gmail.com>
2025-12-29 22:31:15 +0700

    chore: rewrite with laravel and add home page

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,87 @@
+<footer class="bg-accent pt-8 px-4 sm:px-16 pb-20">
+    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
+        <div class="space-y-4">
+            <h1 class="text-secondary font-bold font-playfair-display text-xl sm:text-2xl">
+                The One Course Fashion
+            </h1>
+            <p class="text-muted  text-sm sm:text-base">Transforming fashion dreams into reality through comprehensive education and professional guidance.</p>
+            <div class="flex items-center gap-2 text-muted">
+                <a
+                    href=""
+                    class="hover:text-white duration-300"
+                >
+                    <svg width="18" height="18" viewBox="0 0 18 18" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M8.75684 4.26465C6.27246 4.26465 4.26855 6.26855 4.26855 8.75293C4.26855 11.2373 6.27246 13.2412 8.75684 13.2412C11.2412 13.2412 13.2451 11.2373 13.2451 8.75293C13.2451 6.26855 11.2412 4.26465 8.75684 4.26465ZM8.75684 11.6709C7.15137 11.6709 5.83887 10.3623 5.83887 8.75293C5.83887 7.14355 7.14746 5.83496 8.75684 5.83496C10.3662 5.83496 11.6748 7.14355 11.6748 8.75293C11.6748 10.3623 10.3623 11.6709 8.75684 11.6709ZM14.4756 4.08105C14.4756 4.66309 14.0068 5.12793 13.4287 5.12793C12.8467 5.12793 12.3818 4.65918 12.3818 4.08105C12.3818 3.50293 12.8506 3.03418 13.4287 3.03418C14.0068 3.03418 14.4756 3.50293 14.4756 4.08105ZM17.4482 5.14355C17.3818 3.74121 17.0615 2.49902 16.0342 1.47559C15.0107 0.452148 13.7686 0.131836 12.3662 0.0615234C10.9209 -0.0205078 6.58887 -0.0205078 5.14355 0.0615234C3.74512 0.12793 2.50293 0.448242 1.47559 1.47168C0.448242 2.49512 0.131836 3.7373 0.0615234 5.13965C-0.0205078 6.58496 -0.0205078 10.917 0.0615234 12.3623C0.12793 13.7646 0.448242 15.0068 1.47559 16.0303C2.50293 17.0537 3.74121 17.374 5.14355 17.4443C6.58887 17.5264 10.9209 17.5264 12.3662 17.4443C13.7686 17.3779 15.0107 17.0576 16.0342 16.0303C17.0576 15.0068 17.3779 13.7646 17.4482 12.3623C17.5303 10.917 17.5303 6.58887 17.4482 5.14355ZM15.5811 13.9131C15.2764 14.6787 14.6865 15.2686 13.917 15.5771C12.7646 16.0342 10.0303 15.9287 8.75684 15.9287C7.4834 15.9287 4.74512 16.0303 3.59668 15.5771C2.83105 15.2725 2.24121 14.6826 1.93262 13.9131C1.47559 12.7607 1.58105 10.0264 1.58105 8.75293C1.58105 7.47949 1.47949 4.74121 1.93262 3.59277C2.2373 2.82715 2.82715 2.2373 3.59668 1.92871C4.74902 1.47168 7.4834 1.57715 8.75684 1.57715C10.0303 1.57715 12.7686 1.47559 13.917 1.92871C14.6826 2.2334 15.2725 2.82324 15.5811 3.59277C16.0381 4.74512 15.9326 7.47949 15.9326 8.75293C15.9326 10.0264 16.0381 12.7646 15.5811 13.9131Z"/>
+                    </svg>
+                </a>
+                <a
+                    href=""
+                    class="hover:text-white duration-300"
+                >
+                    <svg width="20" height="20" viewBox="0 0 20 20" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M19.375 9.6875C19.375 4.33594 15.0391 0 9.6875 0C4.33594 0 0 4.33594 0 9.6875C0 14.5227 3.54258 18.5305 8.17383 19.2578V12.4879H5.71289V9.6875H8.17383V7.55312C8.17383 5.12539 9.61914 3.78437 11.8328 3.78437C12.893 3.78437 14.0016 3.97344 14.0016 3.97344V6.35625H12.7797C11.5766 6.35625 11.2012 7.10312 11.2012 7.86914V9.6875H13.8879L13.4582 12.4879H11.2012V19.2578C15.8324 18.5305 19.375 14.5227 19.375 9.6875Z"/>
+                    </svg>
+                </a>
+                <a
+                    href=""
+                    class="hover:text-white duration-300"
+                >
+                    <svg width="22" height="15" viewBox="0 0 22 15" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M20.8876 2.34699C20.6422 1.42316 19.9193 0.695586 19.0015 0.448672C17.3378 0 10.6667 0 10.6667 0C10.6667 0 3.99559 0 2.33187 0.448672C1.41402 0.695625 0.691133 1.42316 0.445781 2.34699C0 4.02148 0 7.51516 0 7.51516C0 7.51516 0 11.0088 0.445781 12.6833C0.691133 13.6071 1.41402 14.3044 2.33187 14.5513C3.99559 15 10.6667 15 10.6667 15C10.6667 15 17.3378 15 19.0015 14.5513C19.9193 14.3044 20.6422 13.6071 20.8876 12.6833C21.3334 11.0088 21.3334 7.51516 21.3334 7.51516C21.3334 7.51516 21.3334 4.02148 20.8876 2.34699ZM8.48484 10.6871V4.34316L14.0606 7.51523L8.48484 10.6871Z"/>
+                    </svg>
+                </a>
+            </div>
+        </div>
+
+        <div class="space-y-4  text-sm sm:text-base">
+            <h1 class="text-white">Program</h1>
+            <ul class="text-muted space-y-2 text-sm sm:text-base">
+                <li class="hover:text-white duration-300">Short Course</li>
+                <li class="hover:text-white duration-300">Regular Program Private</li>
+                <li class="hover:text-white duration-300">Diploma 1 Year</li>
+                <li class="hover:text-white duration-300">About Program</li>
+                <li class="hover:text-white duration-300">Monthly Program</li>
+            </ul>
+        </div>
+
+        <div class="space-y-4  text-sm sm:text-base">
+            <h1 class="text-white">Navigation</h1>
+            <nav>
+                <ul class="text-muted space-y-2">
+                    <li class="hover:text-white duration-300">Home</li>
+                    <li class="hover:text-white duration-300">About</li>
+                    <li class="hover:text-white duration-300">Program</li>
+                    <li class="hover:text-white duration-300">Galeri</li>
+                    <li class="hover:text-white duration-300">Testimoni</li>
+                    <li class="hover:text-white duration-300">Kontak</li>
+                </ul>
+            </nav>
+        </div>
+
+        <div class="space-y-4  text-sm sm:text-base">
+            <h1 class="text-white">Informasi Kontak</h1>
+            <nav>
+                <ul class="text-muted space-y-2">
+                    <li class="flex gap-2 items-center hover:text-white duration-300">
+                        <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M5.15312 0.769455C4.9125 0.188205 4.27812 -0.12117 3.67188 0.0444546L0.921875 0.794455C0.378125 0.944455 0 1.4382 0 2.0007C0 9.73195 6.26875 16.0007 14 16.0007C14.5625 16.0007 15.0563 15.6226 15.2063 15.0788L15.9563 12.3288C16.1219 11.7226 15.8125 11.0882 15.2312 10.8476L12.2312 9.59758C11.7219 9.38508 11.1313 9.53196 10.7844 9.96008L9.52188 11.5007C7.32188 10.4601 5.54062 8.67883 4.5 6.47883L6.04063 5.21945C6.46875 4.86945 6.61562 4.28195 6.40312 3.77258L5.15312 0.77258V0.769455Z" fill="#9CA3AF"/>
+                        </svg>
+                        <span>+62-851-3807-3186</span>
+                    </li>
+                    <li class="flex gap-2 items-center hover:text-white duration-300">
+                        <svg width="16" height="12" viewBox="0 0 16 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M1.5 0C0.671875 0 0 0.671875 0 1.5C0 1.97187 0.221875 2.41562 0.6 2.7L7.4 7.8C7.75625 8.06563 8.24375 8.06563 8.6 7.8L15.4 2.7C15.7781 2.41562 16 1.97187 16 1.5C16 0.671875 15.3281 0 14.5 0H1.5ZM0 3.5V10C0 11.1031 0.896875 12 2 12H14C15.1031 12 16 11.1031 16 10V3.5L9.2 8.6C8.4875 9.13438 7.5125 9.13438 6.8 8.6L0 3.5Z" fill="#9CA3AF"/>
+                        </svg>
+                        <span>theonecoursefashion@gmail.com</span>
+                    </li>
+                    <li class="flex gap-2 items-center hover:text-white duration-300">
+                        <svg width="12" height="16" viewBox="0 0 12 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M6.74062 15.6C8.34375 13.5938 12 8.73125 12 6C12 2.6875 9.3125 0 6 0C2.6875 0 0 2.6875 0 6C0 8.73125 3.65625 13.5938 5.25938 15.6C5.64375 16.0781 6.35625 16.0781 6.74062 15.6ZM6 4C6.53043 4 7.03914 4.21071 7.41421 4.58579C7.78929 4.96086 8 5.46957 8 6C8 6.53043 7.78929 7.03914 7.41421 7.41421C7.03914 7.78929 6.53043 8 6 8C5.46957 8 4.96086 7.78929 4.58579 7.41421C4.21071 7.03914 4 6.53043 4 6C4 5.46957 4.21071 4.96086 4.58579 4.58579C4.96086 4.21071 5.46957 4 6 4Z" fill="#9CA3AF"/>
+                        </svg>
+                        <span>Malang, Indonesia</span>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</footer>
